e2032ed80c1776f2c5e4d520980d128d64a3abf6
Ganesh Kumar <ganeshkumar@Ganeshs-MacBook-Air.local>
2019-10-13 16:16:31 +0530

    emm

--- day_01/script.js
+++ day_01/script.js
@@ -1,10 +1,4 @@
-function is_leap_year (year){if ( year % 400==0 ){return true;}
-
-else if ((year % 4 == 0) && (year % 100!=0)){
-return true;
+function test () {
+    alert('HELLO WORLD');
 }
-else{
-return false;
-}
-}
-alert ( hello world )
+test()
